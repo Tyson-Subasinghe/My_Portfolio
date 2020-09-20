e93331f772f4e5e8a987de970b010cba4a8a3b4c
Tyson <tyson@blufinance.com.au>
2020-09-20 14:49:36 +1000

    Updated build folder

---
+++ build/precache-manifest.36800ccf4ea401258f01780a700e36df.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e138a53e9a5db837412338ae96f2d591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "418c1ccc0b3fa7d9a511",
+    "url": "/static/css/main.27be1118.chunk.css"
+  },
+  {
+    "revision": "165e1da8854e20c96415",
+    "url": "/static/js/2.713efbbb.chunk.js"
+  },
+  {
+    "revision": "f49ff1cb809b7d06de4c6fff713202f7",
+    "url": "/static/js/2.713efbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418c1ccc0b3fa7d9a511",
+    "url": "/static/js/main.14b5f416.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "465ee3228057356003907c37630b9a9b",
+    "url": "/static/media/404.465ee322.svg"
+  },
+  {
+    "revision": "5d6673c4275c725f69700efd0dcdbf20",
+    "url": "/static/media/ITCAvantGardeStd-Demi.5d6673c4.otf"
+  },
+  {
+    "revision": "d6813086d94329f00c2a1a7fab424347",
+    "url": "/static/media/Plateia-Bold.d6813086.ttf"
+  },
+  {
+    "revision": "16fa72751304121500fe7d95c387857b",
+    "url": "/static/media/contact.16fa7275.svg"
+  },
+  {
+    "revision": "c9b749e8e3f5c10edec5c6e8d6df951f",
+    "url": "/static/media/email.c9b749e8.png"
+  },
+  {
+    "revision": "5d85e4aed231073642ce0efeba82f2d6",
+    "url": "/static/media/github.5d85e4ae.png"
+  },
+  {
+    "revision": "10775d93c8602af5c975c3a47ff8949b",
+    "url": "/static/media/history.10775d93.svg"
+  },
+  {
+    "revision": "80ab6fc0db5ba97b29fa024dffc4d1fc",
+    "url": "/static/media/home.80ab6fc0.svg"
+  },
+  {
+    "revision": "dc1b792d08879aa5a6c1dc3256bdfffa",
+    "url": "/static/media/linkedin.dc1b792d.png"
+  },
+  {
+    "revision": "2d341cb2cc9dfd98c14fe7a77fe898f7",
+    "url": "/static/media/passions.2d341cb2.svg"
+  },
+  {
+    "revision": "dbfacd946bb6caf0e352bd04f1ec562a",
+    "url": "/static/media/phone.dbfacd94.png"
+  },
+  {
+    "revision": "0c2dcf8ff4d4726d49704e4d635e559a",
+    "url": "/static/media/projects.0c2dcf8f.svg"
+  },
+  {
+    "revision": "9004b1de5f1c9e711c06067e1a38ac70",
+    "url": "/static/media/tealShoe.9004b1de.png"
+  },
+  {
+    "revision": "825816ea3d1210f8bce7e5a0bd3b6e3b",
+    "url": "/static/media/tealShoeLowRes.825816ea.png"
+  }
+]);
