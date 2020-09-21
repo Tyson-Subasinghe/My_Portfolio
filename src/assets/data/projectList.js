b7155bd7089c50e6ca0b551b70b6db937234fd18
Tyson <tyson@blufinance.com.au>
2020-09-21 23:40:02 +1000

    Added loan repayment calculator to projects

--- src/assets/data/projectList.js
+++ src/assets/data/projectList.js
@@ -1,24 +1,33 @@
 const projectList =[
 
 
+
     {
+        img: 'https://i.ibb.co/xLhXppf/logo.png',
+        subtitle: 'Side Project',
+        title: 'Loan Repayment Calculator',
+        languages: 'ReactJS, CSS',
+        description: 'A cross-browser/cross-device compatible mortgage repayment calculator which balances functionality and simplicity. URL: www.blufinance.com.au',
+        link: 'https://github.com/Tyson-Subasinghe/Blu-Finance-Repayment-Calculator',
+        id: '1',
+      },
+      {
         img: 'https://i.ibb.co/xLhXppf/logo.png',
         subtitle: 'Side Project',
         title: 'TysonTheDev Website',
         languages: 'ReactJS, CSS',
-        description: 'Developing this site has given me exposure to ReactJS, CSS and cross-browser / cross-device development.',
+        description: 'Developing this site has given me exposure to ReactJS, CSS and cross-browser / cross-device development. URL: im.tysonthe.dev',
         link: 'https://github.com/Tyson-Subasinghe/Portfolio',
-        id: '1',
+        id: '2',
       },
-
-    {
+      {
         img: 'https://i.ibb.co/g7Xrftb/meal-Match-Image.png',
         subtitle: 'COMP3900 Project [Scored 84/100]',
         title: 'MealMatch Website',
         languages: 'ReactJS, CSS, Python',
         description: 'A site used to interact with the Spoonacular API, MealMatch lets users find recipes based on ingredients they have in their fridges. The project was completed as part of a team of five people. ',
         link: 'https://github.com/Tyson-Subasinghe/MealMatch',
-        id: '2',
+        id: '3',
       },
       {
         img: 'https://i.ibb.co/nP8YhKN/fnc1-Image.png',
@@ -27,7 +36,7 @@ const projectList =[
         languages: 'Report',
         description: 'A report written on the 2017 Fake News Challenge. Different machine learning techniques and neural networks were employed in an to attempt to beat the winning scores. The report was completed as part of a team of four people. ',
         link: 'https://github.com/Tyson-Subasinghe/Fake_News_Challenge_Report',
-        id: '3',
+        id: '4',
       },
       {
         img: 'https://i.ibb.co/hs1g6GW/dungeon-Game-Image.png',
@@ -36,7 +45,7 @@ const projectList =[
         languages: 'Java',
         description: 'A 2D dungeon crawler game made using Java. The game featured multiple enemy types, power-ups and randomly generated terrain. The project was completed as part of a team of two people. ',
         link: 'https://github.com/Tyson-Subasinghe/Dungeon_Game',
-        id: '4',
+        id: '5',
       },
 
 
